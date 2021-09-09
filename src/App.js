8ceae78c0d3e5731ac18bac455aa2f91167fa44f
thefredo1000 <popobasura3@hotmail.com>
2021-09-09 00:14:17 -0500

    change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react' 
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
+import { Card, Grid, CardActions } from '@material-ui/core';
+import CardHeader from '@material-ui/core/CardHeader';
+import ConnectCard from './components/ConnectCard'
+
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import './App.css'
+
+import PressStart2P from './fonts/PressStart2P-Regular.ttf';
 
 function App() {
+
+  const font = {
+    fontFamily: 'Press Start 2P',
+    fontStyle: 'normal',
+    fontDisplay: 'swap',
+    fontWeight: 400,
+    src: `
+      url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap') format('ttf')
+    `,
+    unicodeRange:
+      'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#d49e17',
+      },
+      secondary: {
+        main: '#FFDD0F',
+      },
+    },
+    typography: {
+      fontFamily: [
+        'Press Start 2P',
+      ],
+    },
+    overrides: {
+      MuiCssBaseline: {
+        '@global': {
+          '@font-face': [font],
+        },
+      },
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path={'/:topicId'}>
+        <br />
+        <Grid container justifyContent="center">
+          <ThemeProvider theme={theme}>
+            <div className="card">
+              <div className="header">
+                <h1>Connect to Hivecraft</h1>
+                <h3>Use your xDAI wallet with Metamask</h3>
+              </div>
+              <br />
+              <ConnectCard />
+            </div>
+          </ThemeProvider>
+        </Grid>
+      </Route>
+    </Router>
   );
 }
 
---
+++ src/components/ConnectCard.js
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from 'react' 
+import Button from '@material-ui/core/Button';
+
+import axios from 'axios'
+
+import { useWallet, UseWalletProvider } from 'use-wallet'
+
+import './ConnectCard.css'
+
+
+
+function ConnectCard () {
+  const wallet = useWallet()
+
+  const [isXDAIChain, setXDAIChain] = useState();
+  const [chainId, setChainId] = useState();
+  const [existInDB, setExistInDB] = useState(false);
+  const [response, setResponse] = useState();
+
+  useEffect(() => {
+    const chainIdData = {
+      jsonrpc: '2.0',
+      method: 'eth_chainId',
+      params: [],
+      id: 0,
+    }
+    window.ethereum.request(chainIdData).then(res => {
+      setXDAIChain(!(res === '0x64'))
+      setChainId(res)
+    })
+  }, []);
+
+
+  function changeChain() {
+    window.ethereum.request({
+      id: 1,
+      jsonrpc: '2.0',
+      method: 'wallet_addEthereumChain',
+      params: [
+        {
+          chainId: '0x64',
+          chainName: 'xDAI Chain',
+          rpcUrls: ['https://dai.poa.network'],
+          iconUrls: [
+            'https://xdaichain.com/fake/example/url/xdai.svg',
+            'https://xdaichain.com/fake/example/url/xdai.png',
+          ],
+          nativeCurrency: {
+            name: 'xDAI',
+            symbol: 'xDAI',
+            decimals: 18,
+          },
+          blockExplorerUrls: ['https://blockscout.com/poa/xdai/'],
+        },
+      ],
+    })
+  }
+
+  window.ethereum.on('chainChanged', (chainId) => setXDAIChain(!(chainId === '0x64')))
+
+  function linkWithMojang (address) {
+    let mojangUUid = window.location.pathname.replace(/^\/+|\/+$/g, '')
+    console.log(mojangUUid)
+    var hnyBal;
+    var plotBal;
+    const hnyAddress  = '0x71850b7E9Ee3f13Ab46d67167341E4bDc905Eef9'
+    const urlHny = `https://blockscout.com/xdai/mainnet/api?module=account&action=tokenbalance&contractaddress=${hnyAddress}&address=${address}`
+    const plotAddress = '0xaa21065406d0b5ec39776b416fe704e6e01bab60'
+    const urlPlot = `https://blockscout.com/xdai/mainnet/api?module=account&action=tokenbalance&contractaddress=${plotAddress}&address=${address}`
+    
+    axios({
+      method: 'get',
+      url: urlHny,
+    })
+    .then(res => {
+      // Gets the balance as wei
+      hnyBal = res.data.result
+      
+      axios({
+        method: 'get',
+        url: urlPlot,
+      })
+      .then(plotRes => {
+        plotBal = plotRes.data.result
+        axios({
+          method: 'post',
+          url: 'https://hivecraft-api.herokuapp.com//user',
+          data: {
+            _id: mojangUUid,
+            mojangId: mojangUUid,
+            walletAddress: address,
+            plotBal: plotBal,
+            hnyBal: hnyBal,
+          }
+        }).then(res => {
+          setResponse('Wallet Connected!')
+          console.log(res)
+        }).catch(err => {
+          console.log(err)
+        })
+      })
+    })
+  }
+
+  function checkIfAlreadyConnected() {
+    axios({
+      method: 'get',
+      url: `https://hivecraft-api.herokuapp.com//user/wallet/${wallet.account}`,
+    })
+    .then(res => {
+      console.log(res)
+      console.log('exist')
+    })
+    .catch(err => {
+      setExistInDB(true)
+    })
+    
+  }
+
+  if (!isXDAIChain) {
+    console.log(wallet.status)
+    if (wallet.status === 'connected') {
+      checkIfAlreadyConnected()
+      if (existInDB){
+        return (
+          <div>
+            <div>{response}</div>
+            {response === 'Wallet Connected!' ? (
+              <></>
+              )
+            : (
+              <Button className="button" onClick={() => linkWithMojang(wallet.account)} variant="contained" color="primary">
+                Link To Hivecraft
+              </Button>
+            )}
+            
+          </div>
+        )
+      } else {
+        console.log(existInDB)
+        return (
+          <div className="error">
+            <p>This wallet is already linked, if you think this is a problem please reach out at our Discord server</p>
+          </div>
+        )
+      }
+    } else {
+      return (
+        <div>
+          <div>{response}</div>
+          <br />
+          <br />
+          <Button className="button" onClick={() => wallet.connect()} variant="contained" color="primary">
+            Connect Your Wallet
+          </Button>
+        </div>
+      )
+    }
+  } else {
+    return (
+      <div>
+        <div>{response}</div>
+        <br />
+        <br />
+        <Button className="button" onClick={changeChain} variant="contained" color="secondary">Change to xDai Network</Button>
+      </div>
+    )
+  }
+}
+
+export default () => (
+  <UseWalletProvider
+    chainId={10}
+  >
+    <ConnectCard />
+  </UseWalletProvider>
+)
